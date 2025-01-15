4643ddbac886ce1ed874b1ad2b3c73df9464024e
Upasana-creator358 <rai.upasna2020@gmail.com>
2025-01-15 10:18:44 +0530

    Create footer.js

---
+++ views/includes/footer.js
@@ -0,0 +1,15 @@
+<footer>
+    <div class="container-footer">
+    <!--Subpart 1 -->
+        <div class="footer-info-social">
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+        </div>
+        <div class="fa-brand-info"> &copy; Airbnb Private Limited</div>
+        <div class="footer-info-terms">
+            <a href="/privacy">privacy</a>
+            <a href="/terms">terms</a>
+        </div>
+    </div>
+</footer>
